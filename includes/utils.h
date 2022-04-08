ffd7914aca70da89906d465cd7b1ce272255736b
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-08 20:54:15 +0530

    dll running perfect

--- includes/utils.h
+++ includes/utils.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-double Max(double a, double b);
-double Min(double a, double b);
+__declspec(dllexport) double Max(double a, double b);
+__declspec(dllexport) double Min(double a, double b);
 void double_to_uint8(double *img, unsigned char *uint8_img, int cols, int rows);
 
 #endif
