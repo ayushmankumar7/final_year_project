da1bc6260b66b5e0d79044cfb645c86b256b805d
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-08 12:29:33 +0530

    documented utils

--- src/utils.c
+++ src/utils.c
@@ -1,15 +1,36 @@
+/*
+* @brief utils file contains all the unitlities functions
+* @author Ayushman Kumar
+* @date 08-04-2022
+*/
+
 #include "../includes/utils.h";
 
+/*
+* @brief Returns the Maximum between 2 double numbers
+* @param a,b double numbers
+* @return Maximum of a and b
+*/
 double Max(double a, double b)
 {
     return a > b ? a : b;
 }
 
+/*
+* @brief Returns the Minimum between 2 double numbers
+* @param a,b double numbers
+* @return Minimum of a and b
+*/
 double Min(double a, double b)
 {
     return a < b ? a : b;
 }
 
+/*
+* @brief Converts a double image to uint8 image
+* @param img double image, output uint8 image, rows, cols of the image   
+* @return void
+*/
 void double_to_uint8(double *img, unsigned char *uint8_img, int cols, int rows){
     int c;
     for (int i = 0; i < rows; i++)
