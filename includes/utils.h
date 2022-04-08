6d3e86bd0cbdf81c6dc19a64566294d0de129215
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-08 11:48:42 +0530

    min max utils added

---
+++ includes/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+double Max(double a, double b);
+double Min(double a, double b);
+
+#endif
---
+++ src/utils.c
@@ -0,0 +1,11 @@
+#include "../includes/utils.h";
+
+double Max(double a, double b)
+{
+    return a > b ? a : b;
+}
+
+double Min(double a, double b)
+{
+    return a < b ? a : b;
+}
