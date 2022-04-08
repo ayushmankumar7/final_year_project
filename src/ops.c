eb044a006a3d5b2028846a3113b3af3baf8b4567
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-08 13:12:50 +0530

    weights added

---
+++ src/ops.c
@@ -0,0 +1,16 @@
+/*
+* @brief Neural Network related Operations included in ops.c
+* @author Ayushman Kumar
+* @date 08-04-2022
+*/
+
+#include "../includes/ops.h"
+
+/*
+* @brief Returns the Maximum between 2 double numbers
+* @param a,b double numbers
+* @return Maximum of a and b
+*/
+void pad_image(double *img, double *img_pad, int rows, int cols, int padsize){
+    
+}
