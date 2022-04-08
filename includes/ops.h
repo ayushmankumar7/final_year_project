d153c94c1d267c3d79cc1b0688f2f5f20a3a8e3c
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-04-08 12:31:59 +0530

    ops header

---
+++ includes/ops.h
@@ -0,0 +1,9 @@
+#ifndef OPS_H
+#define OPS_H
+
+void deconv(double *img_input, double *img_output, double *kernel, int cols, int rows, int stride);
+void pad_image(double *img, double *img_pad, int rows, int cols, int padsize);
+void imfilter(double *img, double *kernel, double *img_fltr, int rows, int cols, int padsize);
+void imadd(double *img_fltr_crnt, double *img_fltr_prev, int cols, int rows);
+
+#endif
